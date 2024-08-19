21e7c6b08ec95cf9175795eb03131df821630cd7
niabalaji <nbalaji1@ucsc.edu>
2024-08-18 18:42:07 -0700

    palindrome

---
+++ 9.palindrome-number.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=9 lang=python
+#
+# [9] Palindrome Number
+#
+
+# @lc code=start
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        left = ""
+        right = ""
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        reversed_half = 0
+        original_x = x
+
+        # Reverse half of the number
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10
+            x //= 10
+
+        # Check for palindrome: reversed_half should match x or        reversed_half without the middle digit
+        return x == reversed_half or x == reversed_half // 10
+        
+# @lc code=end
+
