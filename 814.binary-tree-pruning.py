c5d6408857836a4c8e879e71cd8634c072a5e009
niabalaji <nbalaji1@ucsc.edu>
2024-08-25 19:01:50 -0700

    summaryranges - capitalone

---
+++ 814.binary-tree-pruning.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=814 lang=python
+#
+# [814] Binary Tree Pruning
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pruneTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        
+# @lc code=end
+
