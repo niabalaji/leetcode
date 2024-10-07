850aa17367e8f80c259ccaeac25bc3fbd35fdd61
niabalaji <nbalaji1@ucsc.edu>
2024-10-07 15:17:37 -0700

    14.longest-common-prefix.py

---
+++ 15.3-sum.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=15 lang=python
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        nums = set()
+        
+        while len(nums) == 3:
+            if (nums[0] + nums[1] + nums[2]) == 0:
+                 
+           
+        
+# @lc code=end
+
